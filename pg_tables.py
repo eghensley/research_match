b684807b72ecebde00eb348eca7c8597e8c22bf8
ehens86 <eghensley@email.wm.edu>
2019-02-27 20:19:04 -0500

    psql

--- pg_tables.py
+++ pg_tables.py
@@ -1,21 +1,146 @@
 authors = ['DROP TABLE IF EXISTS research_match.authors;',
+           
            'CREATE TABLE research_match.authors \
             (auth_id integer NOT NULL, \
             name text COLLATE pg_catalog."default" NOT NULL, \
             page text COLLATE pg_catalog."default", \
-            CONSTRAINT authors_pkey PRIMARY KEY (auth_id)) \
+            page_hash bigint NOT NULL, \
+            CONSTRAINT authors_pkey PRIMARY KEY (auth_id), \
+            CONSTRAINT authors_uq_page UNIQUE (page_hash)) \
             WITH (OIDS = FALSE) \
-            TABLESPACE pg_default;']
+            TABLESPACE pg_default;'
+            
+            'DROP INDEX IF EXISTS research_match.auth_page_idx;',
+            'CREATE INDEX auth_page_idx \
+            ON research_match.authors USING btree \
+            (page COLLATE pg_catalog."default" text_pattern_ops) \
+            TABLESPACE pg_default;',
+            
+            'DROP INDEX IF EXISTS research_match.auth_page_hash_idx;',
+            'CREATE INDEX auth_page_hash_idx \
+            ON research_match.authors USING btree \
+            (page_hash) \
+            TABLESPACE pg_default;'
+            ]
+
+papers = ['DROP TABLE IF EXISTS research_match.papers;',
+           'CREATE TABLE research_match.papers \
+            (pap_id integer NOT NULL, \
+            mongo_hash varchar NOT NULL, \
+            page text COLLATE pg_catalog."default" NOT NULL, \
+            page_hash bigint NOT NULL, \
+            CONSTRAINT papers_pkey PRIMARY KEY (pap_id), \
+            CONSTRAINT papers_uq_page UNIQUE (page_hash), \
+            CONSTRAINT papers_uq_mongo UNIQUE (mongo_hash)) \
+            WITH (OIDS = FALSE) \
+            TABLESPACE pg_default;',
+
+            'DROP INDEX IF EXISTS research_match.pap_page_idx;',
+            'CREATE INDEX pap_page_idx \
+            ON research_match.papers USING btree \
+            (page COLLATE pg_catalog."default" text_pattern_ops) \
+            TABLESPACE pg_default;',
+            
+            'DROP INDEX IF EXISTS research_match.pap_page_hash_idx;',
+            'CREATE INDEX pap_page_hash_idx \
+            ON research_match.papers USING btree \
+            (page_hash) \
+            TABLESPACE pg_default;'
+            
+            'DROP INDEX IF EXISTS research_match.pap_mongo_hash_idx;',
+            'CREATE INDEX pap_mongo_hash_idx \
+            ON research_match.papers USING btree \
+            (mongo_hash COLLATE pg_catalog."default") \
+            TABLESPACE pg_default;'
+            ]
 
 
 publishers = ['DROP TABLE IF EXISTS research_match.publishers;',
               'CREATE TABLE research_match.publishers \
             (pub_id integer NOT NULL, \
             name text COLLATE pg_catalog."default" NOT NULL, \
-            page text COLLATE pg_catalog."default", \
-            CONSTRAINT publishers_pkey PRIMARY KEY (pub_id)) \
-            WITH (OIDS = FALSE)']
+            page text COLLATE pg_catalog."default" NOT NULL, \
+            page_hash bigint NOT NULL, \
+            CONSTRAINT publishers_pkey PRIMARY KEY (pub_id), \
+            CONSTRAINT publishers_uq_page UNIQUE (page_hash)) \
+            WITH (OIDS = FALSE)',
+            'DROP INDEX  IF EXISTS research_match.pub_name_idx;',
+            'CREATE INDEX pub_name_idx \
+            ON research_match.publishers USING btree \
+            (name COLLATE pg_catalog."default" text_pattern_ops) \
+            TABLESPACE pg_default;',
+            'DROP INDEX  IF EXISTS research_match.pub_page_idx;',
+            'CREATE INDEX pub_page_idx \
+            ON research_match.publishers USING btree \
+            (page COLLATE pg_catalog."default" text_pattern_ops) \
+            TABLESPACE pg_default;',
             
+            'DROP INDEX IF EXISTS research_match.pub_page_hash_idx;',
+            'CREATE INDEX pub_page_hash_idx \
+            ON research_match.publishers USING btree \
+            (page_hash) \
+            TABLESPACE pg_default;'            
+            ]
+
+
+institutions = ['DROP TABLE IF EXISTS research_match.institutions;',
+              'CREATE TABLE research_match.institutions \
+            (inst_id integer NOT NULL, \
+            name text COLLATE pg_catalog."default" NOT NULL, \
+            page text COLLATE pg_catalog."default" NOT NULL, \
+            page_hash bigint NOT NULL, \
+            CONSTRAINT institutions_pkey PRIMARY KEY (inst_id), \
+            CONSTRAINT institutions_uq_page UNIQUE (page_hash)) \
+            WITH (OIDS = FALSE)',
+            'DROP INDEX  IF EXISTS research_match.inst_name_idx;',
+            'CREATE INDEX inst_name_idx \
+            ON research_match.institutions USING btree \
+            (name COLLATE pg_catalog."default" text_pattern_ops) \
+            TABLESPACE pg_default;',
+            'DROP INDEX  IF EXISTS research_match.inst_page_idx;',
+            'CREATE INDEX inst_page_idx \
+            ON research_match.institutions USING btree \
+            (page COLLATE pg_catalog."default" text_pattern_ops) \
+            TABLESPACE pg_default;',
+
+            'DROP INDEX IF EXISTS research_match.inst_page_hash_idx;',
+            'CREATE INDEX inst_page_hash_idx \
+            ON research_match.institutions USING btree \
+            (page_hash) \
+            TABLESPACE pg_default;'      
+            ]
+
+
+xref_pap_auth = ['DROP TABLE IF EXISTS research_match.xref_pap_auth;',
+              'CREATE TABLE research_match.xref_pap_auth \
+            (xref_pap_auth_id integer NOT NULL, \
+            name text COLLATE pg_catalog."default" NOT NULL, \
+            page text COLLATE pg_catalog."default" NOT NULL, \
+            page_hash bigint NOT NULL, \
+            CONSTRAINT institutions_pkey PRIMARY KEY (inst_id), \
+            CONSTRAINT institutions_uq_page UNIQUE (page_hash)) \
+            WITH (OIDS = FALSE)',
+            'DROP INDEX  IF EXISTS research_match.inst_name_idx;',
+            'CREATE INDEX inst_name_idx \
+            ON research_match.institutions USING btree \
+            (name COLLATE pg_catalog."default" text_pattern_ops) \
+            TABLESPACE pg_default;',
+            'DROP INDEX  IF EXISTS research_match.inst_page_idx;',
+            'CREATE INDEX inst_page_idx \
+            ON research_match.institutions USING btree \
+            (page COLLATE pg_catalog."default" text_pattern_ops) \
+            TABLESPACE pg_default;',
+
+            'DROP INDEX IF EXISTS research_match.inst_page_hash_idx;',
+            'CREATE INDEX inst_page_hash_idx \
+            ON research_match.institutions USING btree \
+            (page_hash) \
+            TABLESPACE pg_default;'      
+            ]
+
+
 create_tables = {}
 create_tables['authors'] = authors
 create_tables['publishers'] = publishers
+create_tables['institutions'] = institutions
+create_tables['papers'] = papers
--- pop_psql.py
+++ pop_psql.py
@@ -14,7 +14,7 @@ import numpy as np
 from pg_tables import create_tables
 
 mongo = _connections.db_connection('mongo')
-psql = _connections.db_connection('psql')
+PSQL = _connections.db_connection('psql')
 
 
 def pg_insert(cur, script):
@@ -44,62 +44,94 @@ def pg_create_table(cur, table_name):
     except Exception as e:
         print("Error: {}".format(str(e)))
 
-def _det_current_pubs(_psql):
+id_translate = {'institutions': 'inst', 'authors': 'auth', 'publishers': 'pub', 'papers': 'pap'}
+
+def _det_current_(_psql, field):
 #    _psql = psql.client
     try:
-        pub_data = pg_query(_psql.client, 'select pub_id, name from research_match.publishers;')
+        _data = pg_query(_psql.client, 'select %s_id, page from research_match.%s;' % (id_translate[field], field))
     except:
         _psql.reset_db_con()
-        pg_create_table(_psql.client, 'publishers')
-        pub_data = pg_query(_psql.client, 'select pub_id, name from research_match.publishers;')
-    if len(pub_data) > 0:
-        current_publishers = set(pub_data[1].values)
-        next_pub_idx = np.max(pub_data[0]) + 1
+        pg_create_table(_psql.client, field)
+        _data = pg_query(_psql.client, 'select %s_id, page from research_match.%s;' % (id_translate[field], field))
+    if len(_data) > 0:
+        current_ = set(_data[1].values)
+        next_idx = np.max(_data[0]) + 1
     else:
-        next_pub_idx = 0
-        current_publishers = set([])
-    return(next_pub_idx, current_publishers, _psql)
-
-def _det_current_auths(_psql):
-    try:
-        auth_data = pg_query(_psql, 'select auth_id, name from research_match.authors;')
-    except:
-        _psql.reset_db_con()
-        pg_create_table(_psql.client, 'authors')
-        auth_data = pg_query(_psql, 'select auth_id, name from research_match.authors;')
-    
-    if len(auth_data) > 0:
-        current_authors = set(auth_data[1].values)
-        next_auth_idx = np.max(auth_data[0]) + 1
-    else:
-        next_auth_idx = 0
-        current_authors = set([])
-    return(next_auth_idx, current_authors)
-
-
-nxt_pub, cur_pubs, psql = _det_current_pubs(psql)
-for entry in mongo.client.find():
-    if 'abstract' not in entry.keys():
-        continue
-    if 'publisher' in entry.keys():
-        if entry['publisher']['name'] not in cur_pubs:
-            pg_insert(psql.client, "insert into research_match.publishers (pub_id, name, page) VALUES (%i, '%s', '%s')" % (nxt_pub, entry['publisher']['name'], entry['publisher']['page']))
-            cur_pubs.add(entry['publisher']['name'])
+        next_idx = 0
+        current_ = set([])
+    return(next_idx, current_, _psql)
+        
+def pop_pubs(psql):
+    nxt_pub, cur_pubs, psql = _det_current_(psql, 'publishers')
+    for entry in mongo.client.find({'abstract': {'$exists': True}, 'publisher': {'$exists': True}}):
+        entry['publisher']['page'] = entry['publisher']['page'].replace('https://www.researchgate.net/', '').lower()
+        if entry['publisher']['page'] not in cur_pubs:
+            script = "insert into research_match.publishers (pub_id, name, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (nxt_pub, entry['publisher']['name'].lower(), entry['publisher']['page'], hash(entry['publisher']['page']))
+            pg_insert(psql.client, script)
+            cur_pubs.add(entry['publisher']['page'])
             nxt_pub += 1
-            
-nxt_auth, cur_auths, psql = _det_current_auths(psql)
-for entry in mongo.find():
-    if 'abstract' not in entry.keys():
-        continue
-    if 'authors' in entry.keys():
+        
+    
+def pop_paps(psql):
+    nxt_pap, cur_paps, psql = _det_current_(psql, 'papers')
+    for entry in mongo.client.find({'abstract': {'$exists': True}}):
+        entry['url_tag'] = entry['url_tag'].lower()
+        if entry['url_tag'] not in cur_paps:
+            script = "insert into research_match.papers (pap_id, mongo_hash, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (nxt_pap, str(entry['_id']), entry['url_tag'], hash(entry['url_tag']))
+            script = script.replace("'NULL'", 'NULL')
+            pg_insert(psql.client, script)
+            cur_paps.add(entry['url_tag'])
+            nxt_pap += 1                
+
+   
+def pop_inst(psql):
+    nxt_inst, cur_insts, psql = _det_current_(psql, 'institutions')
+    for entry in mongo.client.find({'abstract': {'$exists': True}, 'authors': {'$exists': True}}):
         for auth in entry['authors']:
-            if auth['name'] not in cur_auths:
-                if 'page' not in auth.keys():
-                    auth['page'] = "NULL"
-                script = "insert into research_match.authors (auth_id, name, page) VALUES (%i, '%s', '%s')" % (nxt_auth, auth['name'], auth['page'])
+            if 'institution' in auth.keys():
+                inst = auth['institution']
+                if 'page' not in inst.keys():
+                    continue   
+                inst['page'] = inst['page'].replace('https://www.researchgate.net/', '').lower()
+                if inst['page'] not in cur_insts:
+                    script = "insert into research_match.institutions (inst_id, name, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (nxt_inst, inst['name'].lower(), inst['page'], hash(inst['page']))
+                    script = script.replace("'NULL'", 'NULL')
+                    pg_insert(psql.client, script)
+                    cur_insts.add(inst['page'])
+                    nxt_inst += 1                
+
+
+def pop_auth(psql):
+    nxt_auth, cur_auths, psql = _det_current_(psql, 'authors')
+    for entry in mongo.client.find({'abstract': {'$exists': True}, 'authors': {'$exists': True}}):
+        for auth in entry['authors']:
+            if 'page' not in auth.keys():
+                continue 
+            auth['page'] = auth['page'].replace('https://www.researchgate.net/', '').lower()
+    
+            if auth['page'] not in cur_auths:
+                script = "insert into research_match.authors (auth_id, name, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (nxt_auth, auth['name'].lower(), auth['page'], hash(auth['page']))
                 script = script.replace("'NULL'", 'NULL')
                 pg_insert(psql.client, script)
-                cur_auths.add(auth['name'])
+                cur_auths.add(auth['page'])
                 nxt_auth += 1                
-        
+    
+
+
+
+auth_dict = {k:v for k,v in pg_query(PSQL.client, 'select page_hash, auth_id from research_match.authors;').values}
+pap_dict = {k:v for k,v in pg_query(PSQL.client, 'select mongo_hash, pap_id from research_match.papers;').values}
+inst_dict = {k:v for k,v in pg_query(PSQL.client, 'select page_hash, inst_id from research_match.institutions;').values}
+pub_dict = {k:v for k,v in pg_query(PSQL.client, 'select page_hash, pub_id from research_match.publishers;').values}
+
+for entry in mongo.client.find({'abstract': {'$exists': True}}):
+    
+
+
+
+
+
+
+
 
