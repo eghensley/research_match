64159e2276dd6db2b8d1ce81ba8375a94cc1e707
ehens86 <eghensley@email.wm.edu>
2019-03-10 21:48:09 -0400

    psql

--- pg_tables.py
+++ pg_tables.py
@@ -113,34 +113,135 @@ institutions = ['DROP TABLE IF EXISTS research_match.institutions;',
 
 xref_pap_auth = ['DROP TABLE IF EXISTS research_match.xref_pap_auth;',
               'CREATE TABLE research_match.xref_pap_auth \
-            (xref_pap_auth_id integer NOT NULL, \
-            name text COLLATE pg_catalog."default" NOT NULL, \
-            page text COLLATE pg_catalog."default" NOT NULL, \
-            page_hash bigint NOT NULL, \
-            CONSTRAINT institutions_pkey PRIMARY KEY (inst_id), \
-            CONSTRAINT institutions_uq_page UNIQUE (page_hash)) \
+            (pap_auth_id integer NOT NULL, \
+            pap_id integer NOT NULL, \
+            auth_id integer NOT NULL, \
+            CONSTRAINT pap_auth_pkey PRIMARY KEY (pap_auth_id), \
+            CONSTRAINT pap_auth_pap_fkey FOREIGN KEY (pap_id) \
+            REFERENCES research_match.papers (pap_id), \
+            CONSTRAINT pap_auth_auth_fkey FOREIGN KEY (auth_id) \
+            REFERENCES research_match.authors (auth_id)) \
             WITH (OIDS = FALSE)',
-            'DROP INDEX  IF EXISTS research_match.inst_name_idx;',
-            'CREATE INDEX inst_name_idx \
-            ON research_match.institutions USING btree \
-            (name COLLATE pg_catalog."default" text_pattern_ops) \
-            TABLESPACE pg_default;',
-            'DROP INDEX  IF EXISTS research_match.inst_page_idx;',
-            'CREATE INDEX inst_page_idx \
-            ON research_match.institutions USING btree \
-            (page COLLATE pg_catalog."default" text_pattern_ops) \
+            
+            'DROP INDEX  IF EXISTS research_match.pap_auth_id_idx;',
+            'CREATE INDEX pap_auth_id_idx \
+            ON research_match.xref_pap_auth USING btree \
+            (pap_auth_id) \
             TABLESPACE pg_default;',
 
-            'DROP INDEX IF EXISTS research_match.inst_page_hash_idx;',
-            'CREATE INDEX inst_page_hash_idx \
-            ON research_match.institutions USING btree \
-            (page_hash) \
-            TABLESPACE pg_default;'      
+            'DROP INDEX  IF EXISTS research_match.pap_auth_pap_idx;',
+            'CREATE INDEX pap_auth_pap_idx \
+            ON research_match.xref_pap_auth USING btree \
+            (pap_id) \
+            TABLESPACE pg_default;',
+
+            'DROP INDEX  IF EXISTS research_match.pap_auth_auth_idx;',
+            'CREATE INDEX pap_auth_auth_idx \
+            ON research_match.xref_pap_auth USING btree \
+            (auth_id) \
+            TABLESPACE pg_default;',
             ]
 
+xref_pap_pub = ['DROP TABLE IF EXISTS research_match.xref_pap_pub;',
+              'CREATE TABLE research_match.xref_pap_pub \
+            (pap_pub_id integer NOT NULL, \
+            pap_id integer NOT NULL, \
+            pub_id integer NOT NULL, \
+            CONSTRAINT pap_pub_pkey PRIMARY KEY (pap_pub_id), \
+            CONSTRAINT pap_pub_pap_fkey FOREIGN KEY (pap_id) \
+            REFERENCES research_match.papers (pap_id), \
+            CONSTRAINT pap_pub_pub_fkey FOREIGN KEY (pub_id) \
+            REFERENCES research_match.publishers (pub_id)) \
+            WITH (OIDS = FALSE)',
+            
+            'DROP INDEX  IF EXISTS research_match.pap_pub_id_idx;',
+            'CREATE INDEX pap_pub_id_idx \
+            ON research_match.xref_pap_pub USING btree \
+            (pap_pub_id) \
+            TABLESPACE pg_default;',
+
+            'DROP INDEX  IF EXISTS research_match.pap_pub_pap_idx;',
+            'CREATE INDEX pap_pub_pap_idx \
+            ON research_match.xref_pap_pub USING btree \
+            (pap_id) \
+            TABLESPACE pg_default;',
+
+            'DROP INDEX  IF EXISTS research_match.pap_pub_pub_idx;',
+            'CREATE INDEX pap_pub_pub_idx \
+            ON research_match.xref_pap_pub USING btree \
+            (pub_id) \
+            TABLESPACE pg_default;',
+            ]
+
+xref_pap_inst = ['DROP TABLE IF EXISTS research_match.xref_pap_inst;',
+              'CREATE TABLE research_match.xref_pap_inst \
+            (pap_inst_id integer NOT NULL, \
+            pap_id integer NOT NULL, \
+            inst_id integer NOT NULL, \
+            CONSTRAINT pap_inst_pkey PRIMARY KEY (pap_inst_id), \
+            CONSTRAINT pap_inst_pap_fkey FOREIGN KEY (pap_id) \
+            REFERENCES research_match.papers (pap_id), \
+            CONSTRAINT pap_inst_inst_fkey FOREIGN KEY (inst_id) \
+            REFERENCES research_match.institutions (inst_id)) \
+            WITH (OIDS = FALSE)',
+            
+            'DROP INDEX  IF EXISTS research_match.pap_inst_id_idx;',
+            'CREATE INDEX pap_inst_id_idx \
+            ON research_match.xref_pap_inst USING btree \
+            (pap_inst_id) \
+            TABLESPACE pg_default;',
+
+            'DROP INDEX  IF EXISTS research_match.pap_inst_pap_idx;',
+            'CREATE INDEX pap_inst_pap_idx \
+            ON research_match.xref_pap_inst USING btree \
+            (pap_id) \
+            TABLESPACE pg_default;',
+
+            'DROP INDEX  IF EXISTS research_match.pap_inst_inst_idx;',
+            'CREATE INDEX pap_inst_inst_idx \
+            ON research_match.xref_pap_inst USING btree \
+            (inst_id) \
+            TABLESPACE pg_default;',
+            ]
+
+references = ['DROP TABLE IF EXISTS research_match.references;',
+           'CREATE TABLE research_match.references \
+            (ref_id integer NOT NULL, \
+            cit_pap_id integer NOT NULL, \
+            ref_pap_id integer NOT NULL, \
+            CONSTRAINT references_pkey PRIMARY KEY (ref_id), \
+            CONSTRAINT references_ref_fkey FOREIGN KEY (ref_pap_id) \
+            REFERENCES research_match.papers (pap_id), \
+            CONSTRAINT references_cit_fkey FOREIGN KEY (cit_pap_id) \
+            REFERENCES research_match.papers (pap_id)) \
+            WITH (OIDS = FALSE) \
+            TABLESPACE pg_default;',
+            
+            'DROP INDEX IF EXISTS research_match.ref_ref_id_idx;',
+            'CREATE INDEX ref_ref_id_idx \
+            ON research_match.references USING btree \
+            (ref_id) \
+            TABLESPACE pg_default;'
+
+            'DROP INDEX IF EXISTS research_match.ref_cit_pap_idx;',
+            'CREATE INDEX ref_cit_pap_idx \
+            ON research_match.references USING btree \
+            (cit_pap_id) \
+            TABLESPACE pg_default;'      
+            
+            'DROP INDEX IF EXISTS research_match.ref_ref_pap_idx;',
+            'CREATE INDEX ref_ref_pap_idx \
+            ON research_match.references USING btree \
+            (ref_pap_id) \
+            TABLESPACE pg_default;'  
+            ]
 
 create_tables = {}
 create_tables['authors'] = authors
 create_tables['publishers'] = publishers
 create_tables['institutions'] = institutions
 create_tables['papers'] = papers
+create_tables['xref_pap_inst'] = xref_pap_inst
+create_tables['xref_pap_pub'] = xref_pap_pub
+create_tables['xref_pap_auth'] = xref_pap_auth
+create_tables['references'] = references
--- pop_psql.py
+++ pop_psql.py
@@ -12,10 +12,7 @@ import _connections
 import pandas as pd
 import numpy as np
 from pg_tables import create_tables
-
-mongo = _connections.db_connection('mongo')
-PSQL = _connections.db_connection('psql')
-
+#from progress_bar import progress
 
 def pg_insert(cur, script):
     try:
@@ -55,83 +52,140 @@ def _det_current_(_psql, field):
         pg_create_table(_psql.client, field)
         _data = pg_query(_psql.client, 'select %s_id, page from research_match.%s;' % (id_translate[field], field))
     if len(_data) > 0:
-        current_ = set(_data[1].values)
+        current_ = {k:v for v,k in _data.values}
         next_idx = np.max(_data[0]) + 1
     else:
         next_idx = 0
-        current_ = set([])
+        current_ = {}
     return(next_idx, current_, _psql)
         
-def pop_pubs(psql):
-    nxt_pub, cur_pubs, psql = _det_current_(psql, 'publishers')
-    for entry in mongo.client.find({'abstract': {'$exists': True}, 'publisher': {'$exists': True}}):
-        entry['publisher']['page'] = entry['publisher']['page'].replace('https://www.researchgate.net/', '').lower()
-        if entry['publisher']['page'] not in cur_pubs:
-            script = "insert into research_match.publishers (pub_id, name, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (nxt_pub, entry['publisher']['name'].lower(), entry['publisher']['page'], hash(entry['publisher']['page']))
-            pg_insert(psql.client, script)
-            cur_pubs.add(entry['publisher']['page'])
-            nxt_pub += 1
-        
+
+def _det_current_xref(_psql, field):
+#    _psql, field = PSQL, 'xref_pap_inst'
+    try:
+        _data = pg_query(_psql.client, 'select %s_id from research_match.%s;' % (field.replace('xref_',''), field))
+    except:
+        _psql.reset_db_con()
+        pg_create_table(_psql.client, field)
+        _data = pg_query(_psql.client, 'select %s_id from research_match.%s;' % (field.replace('xref_',''), field))
+    if len(_data) > 0:
+        next_idx = np.max(_data[0]) + 1
+    else:
+        next_idx = 0
+    return(next_idx, _psql)
     
-def pop_paps(psql):
+
+def pop_pap(_psql, _nxt_pap, _cur_paps, _entry):
+    script = "insert into research_match.papers (pap_id, mongo_hash, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (_nxt_pap, str(_entry['_id']), _entry['url_tag'], hash(_entry['url_tag']))
+    script = script.replace("'NULL'", 'NULL')
+    pg_insert(_psql.client, script)
+    _cur_paps[_entry['url_tag']] = _nxt_pap
+    _nxt_pap += 1 
+    return(_nxt_pap, _cur_paps)
+    
+def pop_pub(_psql, _nxt_pub, _nxt_pap, _cur_pubs, _entry):
+    script = "insert into research_match.publishers (pub_id, name, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (_nxt_pub, _entry['publisher']['name'].lower(), _entry['publisher']['page'], hash(_entry['publisher']['page']))
+    pg_insert(_psql.client, script)
+    _cur_pubs[_entry['publisher']['page']] = _nxt_pub
+    _nxt_pub += 1      
+    return(_nxt_pub, _cur_pubs)
+ 
+def pop_pap_pub(_psql, _pub_id, _nxt_pap, _nxt_pap_pub):
+    script = "insert into research_match.xref_pap_pub (pap_pub_id, pap_id, pub_id) VALUES (%i, %i, %i)" % (_nxt_pap_pub, _nxt_pap - 1, _pub_id)
+    pg_insert(_psql.client, script)
+    _nxt_pap_pub += 1      
+    return(_nxt_pap_pub)
+
+def pop_auth(_psql, _nxt_auth, _nxt_pap, _cur_auths, _auth):
+    script = "insert into research_match.authors (auth_id, name, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (_nxt_auth, _auth['name'].lower(), _auth['page'], hash(_auth['page']))
+    script = script.replace("'NULL'", 'NULL')
+    pg_insert(_psql.client, script)  
+    _cur_auths[_auth['page']] = _nxt_auth
+    _nxt_auth += 1 
+    return(_nxt_auth, _cur_auths)                
+                
+def pop_pap_auth(_psql, _auth_id, _nxt_pap, _nxt_pap_auth):
+    script = "insert into research_match.xref_pap_auth (pap_auth_id, pap_id, auth_id) VALUES (%i, %i, %i)" % (_nxt_pap_auth, _nxt_pap - 1, _auth_id)
+    pg_insert(_psql.client, script)
+    _nxt_pap_auth += 1    
+    return(_nxt_pap_auth)
+    
+def pop_inst(_psql, _nxt_inst, _nxt_pap, _cur_insts, _inst):
+    script = "insert into research_match.institutions (inst_id, name, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (_nxt_inst, _inst['name'].lower(), _inst['page'], hash(_inst['page']))
+    script = script.replace("'NULL'", 'NULL')
+    pg_insert(_psql.client, script)
+    _cur_insts[_inst['page']] = _nxt_inst
+    _nxt_inst += 1  
+    return(_nxt_inst, _cur_insts)                
+        
+def pop_pap_inst(_psql, _inst_id, _nxt_pap, _nxt_pap_inst):
+    script = "insert into research_match.xref_pap_inst (pap_inst_id, pap_id, inst_id) VALUES (%i, %i, %i)" % (_nxt_pap_inst, _nxt_pap - 1, _inst_id)
+    pg_insert(_psql.client, script)
+    _nxt_pap_inst += 1      
+    return(_nxt_pap_inst)         
+      
+    
+def update_psql(mongo, psql):
     nxt_pap, cur_paps, psql = _det_current_(psql, 'papers')
+    nxt_pub, cur_pubs, psql = _det_current_(psql, 'publishers')
+    nxt_auth, cur_auths, psql = _det_current_(psql, 'authors')
+    nxt_inst, cur_insts, psql = _det_current_(psql, 'institutions')
+    
+    nxt_pap_inst, psql = _det_current_xref(psql, 'xref_pap_inst')
+    nxt_pap_pub, psql = _det_current_xref(psql, 'xref_pap_pub')
+    nxt_pap_auth, psql = _det_current_xref(psql, 'xref_pap_auth')
+    
     for entry in mongo.client.find({'abstract': {'$exists': True}}):
         entry['url_tag'] = entry['url_tag'].lower()
-        if entry['url_tag'] not in cur_paps:
-            script = "insert into research_match.papers (pap_id, mongo_hash, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (nxt_pap, str(entry['_id']), entry['url_tag'], hash(entry['url_tag']))
-            script = script.replace("'NULL'", 'NULL')
-            pg_insert(psql.client, script)
-            cur_paps.add(entry['url_tag'])
-            nxt_pap += 1                
-
-   
-def pop_inst(psql):
-    nxt_inst, cur_insts, psql = _det_current_(psql, 'institutions')
-    for entry in mongo.client.find({'abstract': {'$exists': True}, 'authors': {'$exists': True}}):
-        for auth in entry['authors']:
-            if 'institution' in auth.keys():
-                inst = auth['institution']
-                if 'page' not in inst.keys():
-                    continue   
-                inst['page'] = inst['page'].replace('https://www.researchgate.net/', '').lower()
-                if inst['page'] not in cur_insts:
-                    script = "insert into research_match.institutions (inst_id, name, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (nxt_inst, inst['name'].lower(), inst['page'], hash(inst['page']))
-                    script = script.replace("'NULL'", 'NULL')
-                    pg_insert(psql.client, script)
-                    cur_insts.add(inst['page'])
-                    nxt_inst += 1                
-
-
-def pop_auth(psql):
-    nxt_auth, cur_auths, psql = _det_current_(psql, 'authors')
-    for entry in mongo.client.find({'abstract': {'$exists': True}, 'authors': {'$exists': True}}):
-        for auth in entry['authors']:
-            if 'page' not in auth.keys():
-                continue 
-            auth['page'] = auth['page'].replace('https://www.researchgate.net/', '').lower()
-    
-            if auth['page'] not in cur_auths:
-                script = "insert into research_match.authors (auth_id, name, page, page_hash) VALUES (%i, '%s', '%s', %i)" % (nxt_auth, auth['name'].lower(), auth['page'], hash(auth['page']))
-                script = script.replace("'NULL'", 'NULL')
-                pg_insert(psql.client, script)
-                cur_auths.add(auth['page'])
-                nxt_auth += 1                
-    
+        if entry['url_tag'] not in cur_paps.keys():
+            # Add papers
+            nxt_pap, cur_paps = pop_pap(psql, nxt_pap, cur_paps, entry)
+            
+            # Add publishers
+            if 'publisher' in entry.keys():
+                entry['publisher']['page'] = entry['publisher']['page'].replace('https://www.researchgate.net/', '').lower()
+                if entry['publisher']['page'] not in cur_pubs.keys():
+                    nxt_pub, cur_pubs = pop_pub(psql, nxt_pub, nxt_pap, cur_pubs, entry)
+                nxt_pap_pub = pop_pap_pub(psql, cur_pubs[entry['publisher']['page']], nxt_pap, nxt_pap_pub)
+    #        # Add authors
+            for auth in entry['authors']:
+                if 'page' not in auth.keys():
+                    continue 
+                auth['page'] = auth['page'].replace('https://www.researchgate.net/', '').lower()
+                if auth['page'] not in cur_auths:
+                    nxt_auth, cur_auths = pop_auth(psql, nxt_auth, nxt_pap, cur_auths, auth)
+                nxt_pap_auth = pop_pap_auth(psql, cur_auths[auth['page']], nxt_pap, nxt_pap_auth)
+                # Add institutions
+                if 'institution' in auth.keys():
+                    inst = auth['institution']
+                    if 'page' not in inst.keys():
+                        continue   
+                    inst['page'] = inst['page'].replace('https://www.researchgate.net/', '').lower()
+                    if inst['page'] not in cur_insts.keys():
+                        nxt_inst, cur_insts = pop_inst(psql, nxt_inst, nxt_pap, cur_insts, inst)
+                    nxt_pap_inst = pop_pap_inst(psql, cur_insts[inst['page']], nxt_pap, nxt_pap_inst)
 
 
-
-auth_dict = {k:v for k,v in pg_query(PSQL.client, 'select page_hash, auth_id from research_match.authors;').values}
-pap_dict = {k:v for k,v in pg_query(PSQL.client, 'select mongo_hash, pap_id from research_match.papers;').values}
-inst_dict = {k:v for k,v in pg_query(PSQL.client, 'select page_hash, inst_id from research_match.institutions;').values}
-pub_dict = {k:v for k,v in pg_query(PSQL.client, 'select page_hash, pub_id from research_match.publishers;').values}
-
-for entry in mongo.client.find({'abstract': {'$exists': True}}):
-    
+def update_refs(mongo, psql):
+    nxt_pap, cur_paps, psql = _det_current_(psql, 'papers')
+    pg_create_table(psql.client, 'references')
+    nxt_ref = 0
+    for entry in mongo.client.find({'abstract': {'$exists': True}}):
+        if 'references' in entry.keys():
+            entry['url_tag'] = entry['url_tag'].lower()
+            for ref in entry['references']:
+                if ref.lower() in cur_paps.keys():
+                    script = "insert into research_match.references (ref_id, cit_pap_id, ref_pap_id) VALUES (%i, %i, %i)" % (nxt_ref, cur_paps[entry['url_tag']], cur_paps[ref.lower()])
+                    pg_insert(psql.client, script)    
+                    nxt_ref += 1
+                    
+                    
+MONGO = _connections.db_connection('mongo')
+PSQL = _connections.db_connection('psql')
 
 
 
-
-
-
-
-
+                    
+                
+                
+                
